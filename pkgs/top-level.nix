557e694c0eb48bd90fc81544b9520eaa606d6cff
NotAShelf <raf@notashelf.dev>
2024-03-18 16:55:45 +0300

    pkgs: init mpvScripts under applications/video

--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -4,21 +4,18 @@
   callPackage,
   ...
 }: {
-  /*
-  packages that follow npins entries
-  they can be updated via npins
-  */
+  # packages that follow npins entries
+  # they can be updated via npins
   ani-cli = mkPackage ./applications/misc/ani-cli;
   rat = mkPackage ./applications/misc/rat;
   rofi-calc-wayland = mkPackage ./applications/misc/rofi-calc-wayland;
   rofi-emoji-wayland = mkPackage ./applications/misc/rofi-emoji-wayland;
 
-  /*
-  static packages
-  need manual intervention with each update
-  */
-  mov-cli = callPackage ./applications/misc/mov-cli {};
+  # static packages
+  # need manual intervention with each update
   cloneit = callPackage ./applications/misc/cloneit {};
+  mov-cli = callPackage ./applications/misc/mov-cli {};
+  mpv-history = callPackage ./applications/video/mpv/scripts/save-history {};
   mastodon-bird-ui = callPackage ./applications/social/mastodon-bird-ui {};
   headscale-ui = callPackage ./applications/networking/headscale-ui {};
   reposilite-bin = callPackage ./applications/networking/reposilite-bin {
