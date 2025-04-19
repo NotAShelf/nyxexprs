6ce880fe0ca554e12f23afc76c6352fe28a79271
NotAShelf <raf@notashelf.dev>
2025-04-19 19:37:53 +0300

    foot-git: re-add fullstreen alpha patch

--- pkgs/foot-git/package.nix
+++ pkgs/foot-git/package.nix
@@ -1,4 +1,5 @@
 {
+  withAlphaPatch ? true,
   lib,
   fetchFromGitea,
   foot,
@@ -6,7 +7,7 @@
   date,
   ...
 }:
-foot.overrideAttrs (let
+foot.overrideAttrs (oldAttrs: let
   pin = pins.foot;
 in {
   pname = "foot-transparent";
@@ -19,6 +20,14 @@ in {
     rev = pin.revision;
     sha256 = pin.hash;
   };
+
+  patches =
+    (oldAttrs.patches or [])
+    ++ (lib.optionals withAlphaPatch [
+      # Thank you fazzi :)
+      # <https://codeberg.org/fazzi/foot/commit/bebc6f0ffd0d767f560ee50825a0b0fba197c90f.patch>
+      ./patches/foot_fullscreen_alpha.patch
+    ]);
 
   meta = {
     description = "An auto-upgrading version of FOot to ensure we are always up to dates";
