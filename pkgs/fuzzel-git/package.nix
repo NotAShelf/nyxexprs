b2a33fb770ebd9a0f6378546674fdd3df8def2c2
NotAShelf <raf@notashelf.dev>
2025-06-11 15:42:13 +0300

    fuzzel-git: pin pixman release; fix build

--- pkgs/fuzzel-git/package.nix
+++ pkgs/fuzzel-git/package.nix
@@ -1,27 +1,38 @@
 {
   lib,
-  fetchFromGitea,
-  fuzzel,
   pins,
   date,
+  pixman,
+  fetchurl,
+  fetchFromGitea,
+  fuzzel,
   ...
-}:
-fuzzel.overrideAttrs (let
+}: let
+  # Latest fuzzel release requires Pixman >=0.46.1, but Nixpkgs still ships 0.42
+  pixman_46 = pixman.overrideAttrs {
+    pname = "pixman";
+    version = "0.46.2";
+    src = fetchurl {
+      url = "https://cairographics.org/releases/pixman-0.46.2.tar.gz";
+      hash = "sha256-Pg3lum41aRaUaj2VgZLxVQXcq4UTR3G/6rTOTim71zM=";
+    };
+  };
   pin = pins.fuzzel;
-in {
-  pname = "fuzzel-git";
-  version = "0-unstable-${date}";
-  src = fetchFromGitea {
-    domain = "codeberg.org";
-    owner = "dnkl";
-    repo = "fuzzel";
-    rev = pin.revision;
-    sha256 = pin.hash;
-  };
+in
+  (fuzzel.override {pixman = pixman_46;}).overrideAttrs {
+    pname = "fuzzel-git";
+    version = "0-unstable-${date}";
+    src = fetchFromGitea {
+      domain = "codeberg.org";
+      owner = "dnkl";
+      repo = "fuzzel";
+      rev = pin.revision;
+      sha256 = pin.hash;
+    };
 
-  meta = {
-    description = "Fuzzel app launcher, tracks latest git revision";
-    mainProgram = "fuzzel";
-    maintainers = with lib.maintainers; [NotAShelf];
-  };
-})
+    meta = {
+      description = "Fuzzel app launcher, tracks latest git revision";
+      mainProgram = "fuzzel";
+      maintainers = with lib.maintainers; [NotAShelf];
+    };
+  }
