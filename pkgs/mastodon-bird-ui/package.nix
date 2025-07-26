442f94175be4093fcaa27d65e6b6e57f6377d740
NotAShelf <raf@notashelf.dev>
2025-07-26 16:29:48 +0300

    mastodon-bird-ui: 2.1.1 -> 2.3.3

--- pkgs/mastodon-bird-ui/package.nix
+++ pkgs/mastodon-bird-ui/package.nix
@@ -15,8 +15,8 @@
     src = fetchFromGitHub {
       owner = "ronilaukkarinen";
       repo = "mastodon-bird-ui";
-      tag = "2.1.1";
-      hash = "sha256-WEw9wE+iBCLDDTZjFoDJ3EwKTY92+LyJyDqCIoVXhzk=";
+      tag = "2.3.3";
+      hash = "sha256-ddx9P8eOtUzTDFMF0ZRKyIRujjLL6rFppUswJn40nFU=";
     };
 
     # based on:
@@ -30,19 +30,19 @@
       mv layout-single-column.css mastodon-bird-ui/layout-single-column.scss
       mv layout-multiple-columns.css mastodon-bird-ui/layout-multiple-columns.scss
 
-      echo -e "@import 'contrast/variables';
-      @import 'application';
-      @import 'contrast/diff';
-      @import 'mastodon-bird-ui/layout-single-column.scss';
-      @import 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-contrast.scss
-      echo -e "@import 'mastodon-light/variables';
-      @import 'application';
-      @import 'mastodon-light/diff';
-      @import 'mastodon-bird-ui/layout-single-column.scss';
-      @import 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-light.scss
-      echo -e "@import 'application';
-      @import 'mastodon-bird-ui/layout-single-column.scss';
-      @import 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-dark.scss
+      echo -e "@use 'contrast/variables';
+      @use 'application';
+      @use 'contrast/diff';
+      @use 'mastodon-bird-ui/layout-single-column.scss';
+      @use 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-contrast.scss
+      echo -e "@use 'mastodon-light/variables';
+      @use 'application';
+      @use 'mastodon-light/diff';
+      @use 'mastodon-bird-ui/layout-single-column.scss';
+      @use 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-light.scss
+      echo -e "@use 'application';
+      @use 'mastodon-bird-ui/layout-single-column.scss';
+      @use 'mastodon-bird-ui/layout-multiple-columns.scss';" > mastodon-bird-ui-dark.scss
     '';
   };
 in {
