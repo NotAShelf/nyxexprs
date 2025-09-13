ea03842ae26ec22b3b9f640b82619fa9e2bab8b1
NotAShelf <raf@notashelf.dev>
2025-09-13 20:38:35 +0300

    fuzzel-git: remove unnecessary pixman override
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a69649f93b970c57988d027b412b75d0a61c8

--- pkgs/fuzzel-git/package.nix
+++ pkgs/fuzzel-git/package.nix
@@ -2,24 +2,13 @@
   lib,
   pins,
   date,
-  pixman,
-  fetchurl,
   fetchFromGitea,
   fuzzel,
   ...
 }: let
-  # Latest fuzzel release requires Pixman >=0.46.1, but Nixpkgs still ships 0.42
-  pixman_46 = pixman.overrideAttrs {
-    pname = "pixman";
-    version = "0.46.2";
-    src = fetchurl {
-      url = "https://cairographics.org/releases/pixman-0.46.2.tar.gz";
-      hash = "sha256-Pg3lum41aRaUaj2VgZLxVQXcq4UTR3G/6rTOTim71zM=";
-    };
-  };
   pin = pins.fuzzel;
 in
-  (fuzzel.override {pixman = pixman_46;}).overrideAttrs {
+  fuzzel.overrideAttrs {
     pname = "fuzzel-git";
     version = "0-unstable-${date}";
     src = fetchFromGitea {
